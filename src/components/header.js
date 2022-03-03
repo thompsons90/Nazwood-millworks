d1a7d8e2bdcb6ad63208f78ced1eebe6a1912a3d
dylan <dylanabozarth@gmail.com>
2022-03-02 18:02:18 -0800

    logo and header

---
+++ src/components/header.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+export const Header = () => {
+    return (
+        <div className="container-fluid header">
+            <div className="row">
+                <div className="col-sm-2"><img src="./logo.png" width="200px" height="150px" alt="wordworking" /></div>
+                <div className="col-sm-8"></div>
+                <div className="col-sm-2">Contact us</div>
+ 
+           
+            </div>
+            <div className="row">
+<div className="col-sm-6"><h2> <b> NAZWOOD </b>millworks</h2></div>
+            <div className="col-sm-4">ARTISTRY - ARCHITECTURE - CRAFTSMANSHIP</div>
+            </div>
+            
+        </div>
+    )
+}
