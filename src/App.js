4fcc6e6675a3f160a202bcc9d045af639acca9cf
dylan <dylanabozarth@gmail.com>
2022-03-07 14:43:53 -0800

    document error

--- src/App.js
+++ src/App.js
@@ -7,11 +7,14 @@ import {
   Route
 } from "react-router-dom";
 import { ImageReview } from './components/imageReview';
+import { Login } from './components/login';
 function App() {
   return (
     <BrowserRouter>
     <Routes>
    <Route path="/" element={<FrontPage />} />
+   <Route path="/login" element={<Login />} />
+   <Route path="/imagereview" element={<ImageReview />} />
     </Routes>
     </BrowserRouter>
   );
---
+++ src/components/login.js
@@ -0,0 +1,52 @@
+
+import { Link, Route, Switch, useLocation, withRouter } from "react-router-dom";
+import {useState, useEffect} from 'react'
+
+export const Login = () => {
+    const [hidden, setHidden] = useState(false)
+    const [verified, setVerified] = useState(false)
+   const [userArray, setUserArray] = useState()
+  
+    async function getUserData() {
+        const response = await fetch(
+          "https://us-west-2.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/curb-appeal-thycd/service/getUsers/incoming_webhook/users"
+       ); 
+        const json = await response.json();
+setUserArray(json.Curb[0]);
+        console.log(userArray, 'Array')
+       
+      }
+    const unHide = () => {
+      setHidden(!hidden)
+      getUserData()
+    }
+    const authUser = () => {
+      let username = document.getElementById('username').value
+      let password = document.getElementById('password').value
+     
+      if (Object.values(userArray).includes(username,password)) {
+          
+          setVerified(true)
+      }
+      else {
+         alert('Invalid Username or password')
+      }
+    }
+    return (
+        <div className="loginPage"><h3 className="text-center">Curb Appeal employee login 
+        <br /> The username is 'steve' and the password is 'password' both lowercase, we will change this later</h3>
+        <br />
+     
+        <div className="text-center loginBox">
+            <label>User Name</label>
+            <br />
+            <input placeholder="username" id="username"></input>
+            <br />
+            <label>Password</label>
+            <br />
+            <input placeholder="password" id="password"></input>
+            <br />
+            <button onClick={() => authUser()}>{verified ? <Link to="/imagereview">See Calendar</Link> : 'Login'}</button>
+            </div><h3 className="text-center"> <br /><Link to="/">Back to front page</Link></h3></div>
+    )
+}
