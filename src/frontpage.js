80985bab692aef20abd4dbe4d3dd38b4ebcf3352
dylan <dylanabozarth@gmail.com>
2022-03-12 14:16:02 -0800

    added burns

--- src/frontpage.js
+++ src/frontpage.js
@@ -2,6 +2,7 @@ import React from "react";
 import { AboutUs } from './components/aboutus';
 import { Contact } from './components/contact';
 import { Header } from './components/header';
+import Kenburns from "./components/kenburns";
 import { OurWork } from './components/ourWork';
 import { Reviews } from './components/reviews';
 import {  WoodTypes } from './components/woodtypes';
@@ -11,8 +12,9 @@ export const FrontPage = () => {
     return (
         <div>
             <div className=" container-fluid">
+            <Header /> <Kenburns />
       <div id="work"></div>
-     <Header />
+    
      
      <OurWork />
     <AboutUs /> <WoodTypes />
