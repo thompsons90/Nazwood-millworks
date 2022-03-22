704add6d9421611d4cd0d05f80858eff2cb3f2f9
dylan <dylanabozarth@gmail.com>
2022-03-21 20:08:19 -0700

    image uploading almost done

--- src/components/imageReview.js
+++ src/components/imageReview.js
@@ -1,89 +1,90 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 
 export const ImageReview = () => {
-    const [Curb, setCurb] = useState([]);
-    const [today, setToday] = useState(new Date())
-    const [image, setImage] = useState(null)
-      async function getCalendarInfo() {
-        const response = await fetch(
-          "https://us-west-2.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/curb-appeal-thycd/service/getEvents/incoming_webhook/getEvents"
-        );
-        const json = await response.json();
-        
-        setCurb(json.Curb);
-      //console.log(Curb)
-      }
-    
-    
-     useEffect(() => {
-       getCalendarInfo()
-       
-     }, [])
-    
-     const selectImage = (e) => {
-setImage(e)
-console.log(image)
-     }
-     const scheduleEvent = () => {
-     
-  
-      axios.post(
-        "https://us-west-2.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/curb-appeal-thycd/service/add-display-image/incoming_webhook/add-image",
-        image
-      );
-      alert('Success! We will contact you shortly')
-     
-        
-      
-    };
-    const deleteImage = (_id) => {
-      axios
-        .delete(
-          `https://us-west-2.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/curb-appeal-thycd/service/delete-image/incoming_webhook/delete?_id=${_id}`
-        )
-        .then(() => {
-          console.log('deleted')
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-      }
-    //let stringTime = new Date().toString();
-      if (Curb ) {
-        return (
-          
-          <div className="calendarPage">
+  const [Curb, setCurb] = useState([]);
+  const [today, setToday] = useState(new Date());
+  const [image, setImage] = useState(null);
+  async function getCalendarInfo() {
+    const response = await fetch(
+      "https://us-west-2.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/curb-appeal-thycd/service/getEvents/incoming_webhook/getEvents"
+    );
+    const json = await response.json();
 
-            <Link to='/'>Back to front page </Link>
-            <div onClick={(e) => scheduleEvent(e)}>Submit the photo</div>
-            <h1 className="text-center">Here you can approve or delete images. If you like them hit "Submit" if not, "Delete"</h1>
-            {Curb.map((Curb) => {
-                return (
-                  <div className="container-fluid">
-                    <div className="yes" key={Curb.Name}>
-                   
-                 
-                  
-                    <div className="col-sm-4">
-                   <img src={Curb.image} className="img-fluid" alt="Customer "></img>
-                   </div></div>
-                  <button onClick={(_id) =>deleteImage(Curb._id)} className="btn btn-danger">Delete</button>
-                  <button onClick={(e) => scheduleEvent(e)} className="btn btn-success">Display this image</button>
-                  <button onClick={() => selectImage(Curb.image)} className="btn btn-warning">Select this </button>
-                  </div>
-                );
-              })}
-            
-          </div>
-        );
-      } else if (Curb) {
-        return (
-         
-          <div className="calendarPage">
-           Something's wrong with the data or your internet connection if you see this for more than a couple seconds. 
-          </div>
-        );
-      } 
-    }
+    setCurb(json.Curb);
+    //console.log(Curb)
+  }
+
+  useEffect(() => {
+    getCalendarInfo();
+  }, []);
+
+
+  const sendImage = (_id) => {
+    axios.post(
+      `https://us-west-2.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/curb-appeal-thycd/service/add-display-image/incoming_webhook/add-image?_id=${_id}`
+    );
+    alert("Success! We will contact you shortly");
+  };
+  const deleteImage = (_id) => {
+    axios
+      .delete(
+        `https://us-west-2.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/curb-appeal-thycd/service/delete-image/incoming_webhook/delete?_id=${_id}`
+      )
+      .then(() => {
+        console.log("deleted");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  //let stringTime = new Date().toString();
+  if (Curb) {
+    return (
+      <div className="calendarPage">
+        <Link to="/">Back to front page </Link>
+        <div onClick={(e) => sendImage(e)}>Submit the photo</div>
+        <h1 className="text-center">
+          Here you can approve or delete images. If you like them hit "Submit"
+          if not, "Delete"
+        </h1>
+        {Curb.map((Curb) => {
+          return (
+            <div className="container-fluid">
+              <div className="yes" key={Curb.Name}>
+                <div className="col-sm-4">
+                  <img
+                    src={Curb.image}
+                    className="img-fluid"
+                    alt="Customer "
+                  ></img>
+                </div>
+              </div>
+              <button
+                onClick={(_id) => deleteImage(Curb._id)}
+                className="btn btn-danger"
+              >
+                Delete
+              </button>
+              <button
+                onClick={(_id) => sendImage(Curb._id)}
+                className="btn btn-success"
+              >
+                Display this image
+              </button>
+             
+            </div>
+          );
+        })}
+      </div>
+    );
+  } else if (Curb) {
+    return (
+      <div className="calendarPage">
+        Something's wrong with the data or your internet connection if you see
+        this for more than a couple seconds.
+      </div>
+    );
+  }
+};
--- src/components/reviews.js
+++ src/components/reviews.js
@@ -57,8 +57,10 @@ export const Reviews = () => {
         "https://us-west-2.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/curb-appeal-thycd/service/schedule/incoming_webhook/scheduleEvent",
         data
       )
-      .then((response) => {
-        alert("Success! ");
+      .then(() => {
+        alert("Thank you for your photo! ");
+        setImage(null);
+        
       });
     
   
