3695533aeca7f5f63d9de40cdb857d491fdb1e6d
Dylan <dylanabozarth@gmail.com>
2022-04-07 20:58:19 -0700

    photos and workstart

--- src/components/ourWork.js
+++ src/components/ourWork.js
@@ -1,14 +1,13 @@
 import React from "react";
-import { WorkPopUp } from "../add-ons/workPopUp";
+import Carosuel from "../add-ons/workPopUp";
 export const OurWork = () => {
   return (
     <div className="workPage container-fluid ">
      
         <h2 className="work-text">Services we offer</h2>
-        <p className="work-text">Hover to learn more</p>
         <div className="container-fluid">
         
-         <WorkPopUp /></div>
+        <Carosuel /></div>
     <h4 className="work-text">
         {" "}
         We offer Walnut, White oak, Poplar, Alder, Cherry, Maple, Hickory <br /> 
