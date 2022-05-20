20ca632facec10b810dbdb76ea2a9f9f86a045c4
Scott Thompson <ThompsonS@comoisd.local>
2022-05-20 11:48:29 -0500

    modal added, return of the navbar

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -5,8 +5,60 @@ export const Modal = ({ open, children, onClose }) => {
 
   return (
     <>
-      <button onClick={onClose}>X</button>
-      <h1>Modal</h1>
+      <ul id="nav" className={open === true ? `overlay ` : `hidden`}>
+        <button onClick={onClose}>X</button>
+        <div className="overlay-inner text-center">
+          <div className="">
+            <li class="nav-link">
+              <a
+                data-name="Our Work"
+                className="nav-text"
+                href="#work"
+                onClick={onClose}
+              >
+                Our work
+              </a>
+            </li>
+          </div>
+
+          <div className="">
+            <li class="nav-link">
+              <a
+                data-name="About Us"
+                className="nav-text"
+                href="#about"
+                onClick={onClose}
+              >
+                About Us
+              </a>
+            </li>
+          </div>
+          <div className="">
+            <li class="nav-link">
+              <a
+                data-name="Reviews"
+                className="nav-text"
+                href="#reviews"
+                onClick={onClose}
+              >
+                Reviews
+              </a>
+            </li>
+          </div>
+          <div className="">
+            <li class="nav-link">
+              <a
+                data-name="Contact Us"
+                className="nav-text"
+                href="#contact"
+                onClick={onClose}
+              >
+                Contact Us
+              </a>
+            </li>
+          </div>
+        </div>
+      </ul>
     </>
   );
 };
